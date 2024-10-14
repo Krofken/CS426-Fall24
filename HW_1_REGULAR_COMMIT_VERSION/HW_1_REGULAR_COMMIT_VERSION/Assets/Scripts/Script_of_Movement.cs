42e89ae32385698ac87fa11fec86ac9a0d73eb29
Talha Özer <aluxilium@gmail.com>
2024-10-15 00:31:48 +0300

    2.12
    Created animations

---
+++ HW_1_REGULAR_COMMIT_VERSION/HW_1_REGULAR_COMMIT_VERSION/Assets/Scripts/Script_of_Movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_of_Movement :  MonoBehaviour
+{
+    public float moveSpeed = 8f;   
+    private Rigidbody rb;           
+
+void Start() {
+    
+    rb = GetComponent<Rigidbody>();
+}
+
+void Update() {
+    
+    float moveHorizontal = Input.GetAxis("Horizontal"); 
+    float moveVertical = Input.GetAxis("Vertical");     
+
+    
+    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+    
+    rb.AddForce(movement * moveSpeed);
+}
+}
